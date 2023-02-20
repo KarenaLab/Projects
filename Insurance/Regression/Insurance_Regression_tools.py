ee5cbd01055d65dd6571a5e0e53bb60c34bc4ae0
EKChikui <EKChikui@gmail.com>
2023-02-20 00:20:34 -0300

    Update Insurance_Regression_tools.py

--- Insurance/Regression/Insurance_Regression_tools.py
+++ Insurance/Regression/Insurance_Regression_tools.py
@@ -18,6 +18,7 @@ from color_source import *
 from plot_histogram import *
 from plot_heatmap import *
 from one_hot_encoding import *
+from split_trainval_test import *
 
 
 # Setup/Config
@@ -29,33 +30,7 @@ path_report = os.path.join(path_main, "Report")
 
 # Functions
 cols_numeric = ["age", "bmi", "charges"]
-cols_categoric = ["sex", "children", "smoker", "region"]
-
-def split_trainval_test(DataFrame, trainval_size=80, seed=None):
-    """
-
-
-    """
-    data = DataFrame.copy()
-
-    if(seed != None):
-        np.random.seed(seed)
-
-    if(trainval_size >= 1):
-        trainval_size = trainval_size / 100
-
-    n_cut = int(data.shape[0] * trainval_size)
-
-    # Shuffling entire dataset
-    data = data.sample(frac=1)
-
-    data_trainval = data.iloc[0:n_cut, :].reset_index(drop=True)
-    data_test = data.iloc[n_cut: , :].reset_index(drop=True)
-         
-
-
-    return data_trainval, data_test
-    
+cols_categoric = ["sex", "children", "smoker", "region"]    
 
 
 # Program --------------------------------------------------------------------
@@ -63,7 +38,7 @@ filename = "insurance.csv"
 df = pd.read_csv(filename, sep=",", encoding="utf-8")
 df = one_hot_encoding(df, columns=cols_categoric)
 
-df_trainval, df_test = split_trainval_test(df, trainval_size=.8, seed=18)
+df_trainval, df_test = split_trainval_test(df, train_size=.8, seed=18)
 
 
 
