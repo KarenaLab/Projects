3e400e3e7eaa2fefb7706bee23c9dd6a67b885f7
Seb Karena <EKChikui@gmail.com>
2023-06-24 20:25:40 -0300

    Create autoIndustry_regression.py

---
+++ Auto Industry/autoIndustry_regression.py
@@ -0,0 +1,40 @@
+
+# Libraries
+import os
+
+import numpy as np
+import pandas as pd
+
+
+# Personal modules
+import sys
+sys.path.append(r"C:\python_modules")
+
+from autoindustry_tools import *
+from plot_histogram import *
+from plot_scatterhistlinreg import *
+from plot_boxplot import *
+
+
+# Functions
+
+
+# Setup/Config
+
+
+
+# Program --------------------------------------------------------------
+filename = "auto_industry.csv"
+df = read_csv(filename)
+
+
+# First Model, very simple (without origin and name)
+cols_remove = ["origin", "name"]
+df = df.drop(columns=cols_remove)
+df = dataframe_preparation(df)
+df = units_conversion(df)
+
+# Featuring Engineering
+df["weight_power_ratio"] = df["weight_kg"] / df["horsepower_hp"]
+
+
