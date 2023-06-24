7465268fe279f1f551bba1ef2d882b7b13997158
Seb Karena <EKChikui@gmail.com>
2023-06-24 20:25:44 -0300

    Create autoindustry_tools.py

---
+++ Auto Industry/autoindustry_tools.py
@@ -0,0 +1,54 @@
+
+# Libraries
+import numpy as np
+import pandas as pd
+
+
+
+def read_csv(filename, sep=",", encoding="utf-8"):
+    """
+    
+
+    """
+    data = pd.read_csv(filename, sep=sep, encoding=encoding)
+
+
+    return data
+
+
+def dataframe_preparation(DataFrame, verbose=True):
+    """
+
+
+    """
+    data = DataFrame.copy()
+    
+    data = data.drop_duplicates()
+    data = data.dropna()
+
+
+    return data
+
+
+def units_conversion(DataFrame, verbose=True):
+    """
+
+
+    """
+    data = DataFrame.copy()
+
+    data["consumption_kpl"] = data["mpg"].apply(lambda x: x * 0.42514371)
+    data = data.drop(columns=["mpg"])
+
+    data["displacement_cm3"] = data["displacement"].apply(lambda x: x * 16.387064)
+    data = data.drop(columns=["displacement"])
+
+    data["weight_kg"] = data["weight"].apply(lambda x: x * 0.45359237)
+    data = data.drop(columns=["weight"])
+
+    data = data.rename(columns={"horsepower": "horsepower_hp"})
+    data = data.rename(columns={"acceleration": "acceleration_s"})
+    
+                     
+    return data
+
