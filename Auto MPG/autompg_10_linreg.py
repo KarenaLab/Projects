00182ce7ab297feeb7ff97a1a05a941a6312b890
Seb Karena <EKChikui@gmail.com>
2024-02-02 00:06:32 -0300

    Update autompg_10_linreg.py

--- Auto MPG/autompg_10_linreg.py
+++ Auto MPG/autompg_10_linreg.py
@@ -32,15 +32,14 @@ from autompg_tools import *
 
 # Program --------------------------------------------------------------
 df = load_dataset()
-df = df.drop(columns=["model_year", "origin", "car_name"])
+df = df.drop(columns=["car_name"])
+df = df.drop(columns=["model_year", "origin"])
 
 # Data preparation
 x, y = target_split(df, target="kpl")
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.7, random_state=137)
-
 x_train, x_test = scaler(x_train, x_test, method=StandardScaler())
-
 _, results = regr_linregr(x_train, x_test, y_train, y_test)
 
 print(results)
