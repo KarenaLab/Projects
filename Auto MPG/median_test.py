eb3b92019ae75de04af836c7484e479f0d18d47f
Seb Karena <EKChikui@gmail.com>
2024-02-20 21:29:20 -0300

    Create median_test.py

---
+++ Auto MPG/median_test.py
@@ -0,0 +1,16 @@
+
+import numpy as np
+
+sample = [1, 2, 3, 4, 5, 6, 500]
+
+mean = np.mean(sample)
+median = np.median(sample)
+
+print(f"{mean=}, median={median} \n")
+
+
+# Perform three tests
+# 01 - Starter
+# 02 - Add a number 6 again
+# 03 - Add a number 500 instead 6
+
