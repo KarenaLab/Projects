398f65d721a56ac3af88d099cb179549b36033cc
Seb Karena <EKChikui@gmail.com>
2024-03-08 22:53:59 -0300

    Create diamonds_10_eda.py

---
+++ Diamonds/diamonds_10_eda.py
@@ -0,0 +1,37 @@
+# Name [P063] - Diamonds
+# EDA for the project
+
+
+# Libraries
+import os
+import sys
+from collections import namedtuple
+
+import numpy as np
+import pandas as pd
+import scipy.stats as st
+
+import matplotlib.pyplot as plt
+
+
+# Personal modules
+sys.path.append(r"c:\python_modules")
+
+from diamonds_tools import *
+
+
+# Functions
+
+
+
+# Setup/Config
+
+
+
+# Program --------------------------------------------------------------
+df = load_dataset()
+
+
+
+# end
+
