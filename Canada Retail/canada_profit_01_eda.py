bb667b387d2231b23335adb4bbee1e794f73179e
Seb Karena <EKChikui@gmail.com>
2024-04-15 21:51:21 -0300

    Create canada_profit_01_eda.py

---
+++ Canada Retail/canada_profit_01_eda.py
@@ -0,0 +1,35 @@
+# Name [Pxxx]
+# (optional) Short description of the program/module.
+
+
+# Libraries
+import os
+import sys
+from collections import namedtuple
+
+import numpy as np
+import pandas as pd
+import scipy.stats as st
+
+import matplotlib.pyplot as plt
+
+
+# Personal modules
+sys.path.append(r"c:\python_modules")
+
+
+# Functions
+
+
+
+# Setup/Config
+
+
+
+# Program --------------------------------------------------------------
+
+
+
+
+# end
+
