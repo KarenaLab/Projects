3e40a146b8979f0e366c276cb949fd66083dde66
Seb Karena <EKChikui@gmail.com>
2024-04-16 20:04:56 -0300

    Update canada_profit_01_eda.py

--- Canada Retail/canada_profit_01_eda.py
+++ Canada Retail/canada_profit_01_eda.py
@@ -1,5 +1,4 @@
-# Name [Pxxx]
-# (optional) Short description of the program/module.
+# Name [P421] - Canada retail profit (Classification)
 
 
 # Libraries
@@ -17,6 +16,8 @@ import matplotlib.pyplot as plt
 # Personal modules
 sys.path.append(r"c:\python_modules")
 
+from canada_profit_tools import *
+
 
 # Functions
 
@@ -27,7 +28,47 @@ sys.path.append(r"c:\python_modules")
 
 
 # Program --------------------------------------------------------------
+df = load_dataset()
 
+"""
+for col in cat_cols():
+    data = df[col].value_counts()
+    x = np.array(data.index)
+    y = list(data.values)
+    plot_barv(x, height=y, title=f"ecommerce - Category - {col}", savefig=True)
+    
+for col in num_cols():
+    data = df[col]
+    plot_histbox(data, title=f"ecommerce - Histogram - {col}", savefig=True)
+
+"""
+
+"""
+# Numerical to Categoric
+df["sales_cat"] = numeric_to_categoric(df["sales"], no_cat=10)
+df = df.drop(columns=["sales"])
+
+df["order_quantity_cat"] = numeric_to_categoric(df["order_quantity"], no_cat=10)
+df = df.drop(columns=["order_quantity"])
+
+# Train/Test split
+x, y = split_target(df, target="profit")
+x = pd.get_dummies(x, drop_first=True)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1)
+
+# Model
+regr = DecisionTreeRegressor()
+
+# Hyperparameters
+
+
+# Fit and predict
+regr.fit(x_train, y_train)
+y_pred = regr.predict(x_test)
+metrics = regr_metrics(y_test, y_pred)
+print(metrics)
+"""
 
 
 
