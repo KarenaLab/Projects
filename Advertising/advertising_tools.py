724685e692d85db695798d02aa03ca96ea992a90
Seb Karena <EKChikui@gmail.com>
2024-05-08 22:30:45 -0300

    Create advertising_tools.py

---
+++ Advertising/advertising_tools.py
@@ -0,0 +1,33 @@
+# Name [Pxxx]
+# (optional) Short description
+
+# Versions
+# 01 - Date - Starter
+# 02 -
+
+
+# Insights, improvements and bugfix
+#
+
+
+# Libraries
+from collections import namedtuple
+
+import numpy as np
+import pandas as pd
+import scipy.stats as st
+
+import matplotlib.pyplot as plt
+
+
+
+# ----------------------------------------------------------------------
+def name():
+    """
+    Description of the function
+    Information about variables and **kwargs
+
+    """
+
+    return None    
+
