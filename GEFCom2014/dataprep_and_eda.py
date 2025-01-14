b743767503d98d69fca2c7ab1263c1d909b0e874
Seb Karena <EKChikui@gmail.com>
2025-01-14 20:59:58 -0300

    add function: `mape`

--- GEFCom2014/dataprep_and_eda.py
+++ GEFCom2014/dataprep_and_eda.py
@@ -117,6 +117,27 @@ def check_nans(DataFrame, columns="all", decimals=2, verbose=True):
     # into a plot
     
     return nans_register
+
+
+def mape(y_true, y_pred):
+    """
+    Mean Absolute Percentage Error (MAPE) stands for the percentual
+    absolute error of the prediction (or forecast).
+    
+    More info:
+    https://en.wikipedia.org/wiki/Mean_absolute_percentage_error
+    https://www.statology.org/how-to-interpret-mape/
+
+    """
+    # Data preparation
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+
+    # Calculation
+    mape = np.mean((np.abs(y_pred - y_true) / y_true))
+    
+
+    return mape
          
 
 # Program --------------------------------------------------------------
