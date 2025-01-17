c137ea9e59214e53f603dc3cfebfa7499c9c588c
Seb Karena <EKChikui@gmail.com>
2025-01-17 20:45:18 -0300

    Update dataprep_and_eda.py

--- GEFCom2014/dataprep_and_eda.py
+++ GEFCom2014/dataprep_and_eda.py
@@ -225,7 +225,73 @@ def coerce_nan(value, kind, function):
         answer = np.nan
 
     return answer
-   
+
+
+def scaler_standard(Series):
+    """
+    Performs a Standard normalization, rescaling data for values with
+    mean in zero and standard deviatin of 1.
+
+    Returns:
+    values = Pandas series with standard scaler applied.
+    params = Python dictionary with value to invert the scaling.
+             params[col_name] = [method, mean, stddev] 
+
+    """
+    # Standard scaling
+    x_mean = Series.mean()
+    x_stddev = Series.std()
+
+    params = dict()
+    params[Series.name] = ["standard", x_mean, x_stddev]
+
+    def standard(x, mean, stddev):
+        x_scaled = (x - mean) / stddev
+        return x_scaled
+
+    Series = Series.apply(lambda x: standard(x, x_mean, x_stddev))
+
+    return Series, params 
+        
+
+def inv_scaler_minmax(Series, params):
+    """
+    Performs the inverse of Min-Max normalization, rescaling the data
+    for the previous (real) data.
+
+    Returns:
+    values = Pandas series with inverse min-max applied.
+
+    """   
+    _, x_min, x_max = list(params.values())[0]
+
+    def inv_minmax(x_scaled, minimum, maximum):
+        x = ((maximum - minimum) * x_scaled) + minimum
+        return x
+
+    Series = Series.apply(lambda xs: inv_minmax(xs, x_min, x_max))
+
+    return Series
+
+
+def inv_scaler_standard(Series, params):
+    """
+    Performs the inverse of Standard normalization, recaling the data
+    for the previous (real) data.
+
+    Return values = Pandas series with inverse standard applied.
+
+    """
+    _, x_mean, x_stddev = list(params.values())[0]
+
+    def inv_standard(x_scaled, mean, stddev):
+        x = (x_scaled * stddev) + mean
+        return x
+
+    Series = Series.apply(lambda xs: inv_standard(xs, x_mean, x_stddev))
+
+    return Series
+
     
 # Program --------------------------------------------------------------
 df = load_dataset()
