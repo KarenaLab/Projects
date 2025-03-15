a71a2938f20d6fc2c86575ed6e0f9eda8049f15a
Seb Karena <EKChikui@gmail.com>
2025-03-15 10:27:35 -0300

    Update concrete_tools.py

--- Cement Strength/concrete_tools.py
+++ Cement Strength/concrete_tools.py
@@ -1,31 +1,19 @@
-# [Pxxx] Project name
-# (optional) Short description
-
-# Versions
-# 01 - Date - Starter
-# 02 -
-
-
-# Insights, improvements and bugfix
-#
+# [P504] Concrete compressive strength
 
 
 # Libraries
 import numpy as np
 import pandas as pd
-import scipy.stats as st
+import scipy.stats as stats
 
 import matplotlib.pyplot as plt
 
 
 
 # ----------------------------------------------------------------------
-def name():
-    """
-    Description of the function
-    Information about variables and **kwargs
+def load_dataset():
+    filename = "concrete_strength.csv"
+    data = pd.read_csv(filename, sep=",", encoding="utf-8")
 
-    """
-
-    return None    
+    return data
 
