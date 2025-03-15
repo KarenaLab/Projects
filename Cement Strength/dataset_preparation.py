8783e0a494d752aecc86892abad8b2bd2914a9a6
Seb Karena <EKChikui@gmail.com>
2025-03-15 09:16:12 -0300

    Create dataset_preparation.py

---
+++ Cement Strength/dataset_preparation.py
@@ -0,0 +1,33 @@
+# [Pxxx] Project name
+# (optional) Short description of the program/module.
+
+
+# Libraries
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+import scipy.stats as st
+
+import matplotlib.pyplot as plt
+
+
+# Personal modules
+sys.path.append(r"c:\python_modules")
+
+
+# Functions
+
+
+
+# Setup/Config
+
+
+
+# Program --------------------------------------------------------------
+
+
+
+
+# end
