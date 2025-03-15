7935d19e90715a582567508909b43fed20f4fed9
Seb Karena <EKChikui@gmail.com>
2025-03-15 10:11:30 -0300

    Create concrete_tools.py

---
+++ Cement Strength/concrete_tools.py
@@ -0,0 +1,31 @@
+# [Pxxx] Project name
+# (optional) Short description
+
+# Versions
+# 01 - Date - Starter
+# 02 -
+
+
+# Insights, improvements and bugfix
+#
+
+
+# Libraries
+import numpy as np
+import pandas as pd
+import scipy.stats as st
+
+import matplotlib.pyplot as plt
+
+
+
+# ----------------------------------------------------------------------
+def name():
+    """
+    Description of the function
+    Information about variables and **kwargs
+
+    """
+
+    return None    
+
