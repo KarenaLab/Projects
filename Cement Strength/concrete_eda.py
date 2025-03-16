d07dcfce5dcda4f25c2bc0d3d577461082362db4
Seb Karena <EKChikui@gmail.com>
2025-03-16 14:36:54 -0300

    Create concrete_eda.py

---
+++ Cement Strength/concrete_eda.py
@@ -0,0 +1,36 @@
+# [P504] Cement compressive strength
+# Performs EDA (Exploratory Data Analysis)
+
+# Libraries
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+
+import matplotlib.pyplot as plt
+
+
+# Personal modules
+sys.path.append(r"c:\python_modules")
+
+
+from concrete_tools import load_dataset
+
+
+# Functions
+
+
+
+# Setup/Config
+
+
+
+# Program --------------------------------------------------------------
+df = load_dataset()
+
+
+
+
+# end
