e887ebf1f2ef9e97b9437c18b42c3d46d97621ab
Seb Karena <EKChikui@gmail.com>
2025-03-29 21:09:01 -0300

    Create concrete_models.py

---
+++ Cement Strength/concrete_models.py
@@ -0,0 +1,47 @@
+# [P504] Cement compressive strength
+# Performs EDA (Exploratory Data Analysis)
+
+# Libraries
+import os
+
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+
+import matplotlib.pyplot as plt
+
+
+# Personal modules
+from concrete_tools import load_dataset
+
+
+# Functions
+def cols_variable():
+    cols = ["cement_kg_p_m3", "blast_furnace_slag_kg_p_m3",
+            "fly_ash_kg_p_m3", "water_kg_p_m3", "superplasticizer_kg_p_m3",
+            "coarse_aggregate_kg_p_m3", "fine_aggregate_kg_p_m3", "age_days"]
+
+    return cols
+
+
+# Setup/Config
+
+
+
+
+# Program --------------------------------------------------------------
+df = load_dataset()
+target = "compressive_strength_mpa"
+
+# Univariate analysis
+
+# Bivariate analysis
+
+# Variables versus target
+
+# Heatmap
+
+# Insights
+
+
+# end
