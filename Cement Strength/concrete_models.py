59ca53e792ae8d1778159683c862c91e818b4f30
Seb Karena <EKChikui@gmail.com>
2025-03-30 22:36:52 -0300

    Update concrete_models.py

--- Cement Strength/concrete_models.py
+++ Cement Strength/concrete_models.py
@@ -8,12 +8,22 @@ import numpy as np
 import pandas as pd
 import scipy.stats as stats
 
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import (r2_score, mean_absolute_error)
+
+
 import matplotlib.pyplot as plt
 
 
 # Personal modules
 from concrete_tools import load_dataset
+
 from src.split_train_test import split_train_test
+from src.stratified_continuous_kfold import stratified_continuous_kfold
+
+from src.plot_histbox import plot_histbox
+
 
 # Functions
 def cols_variable():
@@ -27,7 +37,57 @@ def cols_variable():
 # Setup/Config
 
 
+def prep_pipeline(DataFrame, train_index, test_index, target):
+    """
 
+
+    """
+    # Split Train and Test with target
+    cols_vars = list(DataFrame.columns)
+    cols_vars.remove(target)
+
+    x_train = DataFrame.loc[train_index, cols_vars]
+    y_train = DataFrame.loc[train_index, target]
+
+    x_test = DataFrame.loc[test_index, cols_vars]
+    y_test = DataFrame.loc[test_index, target]
+
+    # Standard Scaler
+    scaler = StandardScaler()
+    scaler.fit(x_train)
+
+    x_train = scaler.transform(x_train)
+    x_test = scaler.transform(x_test)
+
+    # Store the params of scaler to export, if it turns a production model
+    
+    # Model: Linear Regression
+    regr = LinearRegression()
+
+    # Parameters
+    regr.fit_intercept = True
+    regr.positive = False
+    
+    # Applying models
+    regr.fit(X=x_train, y=y_train)
+
+    # Train Score
+
+    # Predictions
+    y_pred = regr.predict(X=x_test)
+
+    # Test Score: R2, Pearson, MAE, RMSE, Bland-Altman
+    r2 = r2_score(y_test, y_pred)
+    mae = mean_absolute_error(y_test, y_pred)
+
+    # Outout
+    hyperparams, params, results = dict(), dict(), dict()
+
+    results["R2 Score"] = r2
+    results["MAE"] = mae
+
+    return hyperparams, params, results
+    
 
 # Program --------------------------------------------------------------
 df = load_dataset()
@@ -35,9 +95,12 @@ target = "compressive_strength_mpa"
 
 # Train/Validation/Test strategy
 trainval, test = split_train_test(df, train_size=70, seed=314)
+skf_folds = stratified_continuous_kfold(trainval, target=target) 
+#           (fold_no, train_index, validation_index)
 
-
-
+for (i, train_index, test_index) in skf_folds:
+    _, _, results = prep_pipeline(df, train_index, test_index, target)
+    print(results)
 
 
 # end
