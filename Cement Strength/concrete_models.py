52fa3127b67379cf746b299bead07d1c4edae02c
Seb Karena <EKChikui@gmail.com>
2025-04-01 22:30:53 -0300

    Update concrete_models.py

--- Cement Strength/concrete_models.py
+++ Cement Strength/concrete_models.py
@@ -17,7 +17,8 @@ import matplotlib.pyplot as plt
 
 
 # Personal modules
-from concrete_tools import load_dataset, cols_variable, prep_pipeline
+from concrete_tools import (load_dataset, cols_variable, prep_pipeline,
+                            aggregate_results)
 
 from src.split_train_test import split_train_test
 from src.stratified_continuous_kfold import stratified_continuous_kfold
@@ -27,12 +28,10 @@ from src.plot_histbox import plot_histbox
 
 # Functions
 
-
-
+        
 # Setup/Config
 
     
-
 # Program --------------------------------------------------------------
 df = load_dataset()
 target = "compressive_strength_mpa"
@@ -42,9 +41,14 @@ trainval, test = split_train_test(df, train_size=70, seed=314)
 skf_folds = stratified_continuous_kfold(trainval, target=target) 
 #           (fold_no, train_index, validation_index)
 
+
+folds_results = pd.DataFrame(data=[])
+
 for (i, train_index, test_index) in skf_folds:
     _, _, results = prep_pipeline(df, train_index, test_index, target)
-    print(results)
+
+    results["fold"] = i
+    fold_results = aggregate_results(folds_results, results)
 
 
 # end
