c7118fd3442c4f46daaff3a8ea77adc679a88aff
Seb Karena <EKChikui@gmail.com>
2025-04-05 22:47:44 -0300

    Update concrete_eda.py

--- Cement Strength/concrete_eda.py
+++ Cement Strength/concrete_eda.py
@@ -16,15 +16,25 @@ from concrete_tools import load_dataset
 
 
 # Functions
-def cols_variable():
+def cols_numerical():
     cols = ["cement_kg_p_m3", "blast_furnace_slag_kg_p_m3",
             "fly_ash_kg_p_m3", "water_kg_p_m3", "superplasticizer_kg_p_m3",
-            "coarse_aggregate_kg_p_m3", "fine_aggregate_kg_p_m3", "age_days"]
+            "coarse_aggregate_kg_p_m3", "fine_aggregate_kg_p_m3"]
 
     return cols
 
 
+def cols_categorical():
+    cols = ["age_days"]
 
+    return cols
+
+
+def cols_variable():
+    cols = cols_numerical() + cols_categorical()
+
+    return cols
+    
 
 # Setup/Config
 
@@ -35,12 +45,20 @@ df = load_dataset()
 target = "compressive_strength_mpa"
 
 # Univariate analysis
+for col in cols_numerical():
+    pass
+
+for col in cols_categorical():
+    pass
 
 # Bivariate analysis
 
+
 # Variables versus target
 
+
 # Heatmap
+
 
 # Insights
 
