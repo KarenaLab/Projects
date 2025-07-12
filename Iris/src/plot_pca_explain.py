dbeb4cecf4d8909e6a66d191817fc9aa3aaab826
Seb Karena <EKChikui@gmail.com>
2025-07-12 11:11:51 -0300

    Create plot_pca_explain.py

---
+++ Iris/src/plot_pca_explain.py
@@ -0,0 +1,79 @@
+# PCA Explainability [P521] ---------------------------------------------
+
+# Libraries
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+
+# -----------------------------------------------------------------------
+
+def plot_pca_explain(array, title=None, color_bar="navy", color_line="darkred",
+                     threshold=95, remove_axis=False, xrotation=False,
+                     savefig=False, verbose=True):
+    """
+
+
+    """
+    # Data preparation
+    array = np.array(array)
+    array = array * 100    
+    cumulative = np.cumsum(array)
+    labels = [f"PC{i}" for i in range(1, (array.size + 1))]
+
+    # Title
+    if(title == None):
+        title = "PCA Explainability"
+
+    # RC Params
+    plt.rcParams["font.family"] = "Helvetica"
+    plt.rcParams["font.size"] = 8
+    plt.rcParams["figure.dpi"] = 120
+    plt.rcParams["ps.papersize"] = "A4"
+    plt.rcParams["xtick.direction"] = "inout"
+    plt.rcParams["ytick.direction"] = "inout"
+    plt.rcParams["xtick.major.size"] = 0
+    plt.rcParams["ytick.major.size"] = 3.5
+
+
+    # Plot
+    fig = plt.figure(figsize=[6, 3.375])        # Widescreen [16:9]
+    fig.suptitle(title, fontsize=10, fontweight="bold", x=0.98, ha="right")
+    ax = plt.axes()
+
+    plt.bar(labels, height=array, color=color_bar, edgecolor="black", label="Explained variance",zorder=20)
+    plt.plot(labels, cumulative, color=color_line, label="Cumulative explainance", zorder=21)
+    plt.axhline(y=threshold, color="darkgreen", linestyle="--", linewidth=0.5, label=f"threshold {threshold}%", zorder=19)
+
+    plt.grid(axis="y", color="lightgrey", linestyle="--", linewidth=0.5, zorder=10)
+    plt.ylabel("%", loc="top")
+
+    plt.legend(loc="lower center", bbox_to_anchor=(0.5, -0.2), ncol=3)
+
+    if(xrotation == True):
+        plt.xticks(rotation=90)       
+
+    if(remove_axis == True):
+        plt.tick_params(length=0, labelleft="on", labelbottom="on")
+        ax.spines.right.set_visible(False)
+        ax.spines.top.set_visible(False)
+        ax.spines.left.set_visible(False)
+
+    # Printing
+    plt.tight_layout()
+
+    if(savefig == True):
+        plt.savefig(title, dpi=320)
+        if(verbose == True):
+            print(f' > saved plot as "{title}.png"')
+
+    else:
+        plt.show()
+
+
+    plt.close(fig)   
+
+
+    return None
+
