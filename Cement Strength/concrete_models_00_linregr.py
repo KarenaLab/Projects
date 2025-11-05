86a01a652a5c8a699b9ce454c77ef3165f1eb9d6
Seb Karena <EKChikui@gmail.com>
2025-11-04 23:01:42 -0300

    Update concrete_models_00_linregr.py

--- Cement Strength/concrete_models_00_linregr.py
+++ Cement Strength/concrete_models_00_linregr.py
@@ -20,7 +20,11 @@ from concrete_tools import (load_dataset, cols_variable, prep_pipeline,
 
 
 # Functions
+def split_target(DataFrame, target):
+    x = DataFrame.drop(columns=[target])
+    y = DataFrame[target]
 
+    return x, y
         
         
 # Setup/Config
@@ -31,4 +35,5 @@ from concrete_tools import (load_dataset, cols_variable, prep_pipeline,
 df = load_dataset()
 target = "compressive_strength_mpa"
 
-
+x, y = split_target(df, target=target)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30, random_state=53)
