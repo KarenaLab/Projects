74721a5143fb5158367b41342f1a495ecf2aa1cb
Seb Karena <EKChikui@gmail.com>
2025-11-16 22:26:46 -0300

    Create penguins_00_eda.py

---
+++ Penguins/penguins_00_eda.py
@@ -0,0 +1,30 @@
+# [P529] Penguins project
+
+# Libraries
+import os
+
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+
+import matplotlib.pyplot as plt
+
+
+# Personal modules
+from src.penguins_tools import load_csv
+
+
+# Functions
+
+
+
+# Setup/Config
+
+
+
+# Program --------------------------------------------------------------
+df = load_csv()
+
+
+
+# end
