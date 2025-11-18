e5c164b13d854c77a7ccf76a588256f7e42dedaf
Seb Karena <EKChikui@gmail.com>
2025-11-18 20:20:50 -0300

    Create loan_payback_tools.py

---
+++ Loan Payback/src/loan_payback_tools.py
@@ -0,0 +1,25 @@
+# [Pxxx] Project name
+# (optional) Short description
+
+# Insights, improvements and bugfix
+#
+
+
+# Libraries
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+
+import matplotlib.pyplot as plt
+
+
+# ----------------------------------------------------------------------
+def name():
+    """
+    Description of the function
+    Information about variables and **kwargs
+
+    """
+
+    return None    
+
