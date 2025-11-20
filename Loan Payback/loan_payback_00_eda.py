4e47302457c871bb9e89e60c12efb7a19153a5ce
Seb Karena <EKChikui@gmail.com>
2025-11-20 00:01:24 -0300

    Update loan_payback_00_eda.py

--- Loan Payback/loan_payback_00_eda.py
+++ Loan Payback/loan_payback_00_eda.py
@@ -19,19 +19,25 @@ import matplotlib.pyplot as plt
 from src.loan_payback_tools import (load_dataset,
                                     cols_categoric, cols_numeric)
 
+from src.plot_histbox import plot_histbox
+
 
 # Functions
 
 
 
 # Setup/Config
-
+savefig = True
 
 
 # Program --------------------------------------------------------------
 df = load_dataset(filename="train.csv", path=".\database")
 target = "loan_paid_back"
 
+sample = df.sample(n=10000, random_state=314)
+for col in cols_numeric():
+    plot_histbox(data=sample[col], title=f"Loan payback S5E11 - Histogram {col}",
+                 savefig=savefig)
 
 
 # end
