3de925768c82e2620f4e52ccb32473dce1320d20
Seb Karena <EKChikui@gmail.com>
2025-11-20 00:23:44 -0300

    Update loan_payback_00_eda.py

--- Loan Payback/loan_payback_00_eda.py
+++ Loan Payback/loan_payback_00_eda.py
@@ -7,6 +7,7 @@
 
 # Libraries
 import os
+import itertools
 
 import numpy as np
 import pandas as pd
@@ -20,6 +21,7 @@ from src.loan_payback_tools import (load_dataset, organize_report,
                                     cols_categoric, cols_numeric)
 
 from src.plot_histbox import plot_histbox
+from src.plot_scatterhist import plot_scatterhist
 
 
 # Functions
@@ -35,10 +37,26 @@ df = load_dataset(filename="train.csv", path=".\database")
 target = "loan_paid_back"
 
 sample = df.sample(n=10000, random_state=314)
+
+"""
+# Univariate analysis
 for col in cols_numeric():
     plot_histbox(data=sample[col], title=f"Loan payback S5E11 - Histogram {col}",
                  savefig=savefig)
 
+for col in cols_categoric():
+    pass
+
+"""
+# Bivariate analysis
+var_comb = list(itertools.combinations(cols_numeric(), 2))
+
+for var_x, var_y in var_comb:
+    plot_scatterhist(x=sample[var_x], xlabel=var_x, y=sample[var_y], ylabel=var_y,
+                     mark_size=15, title=f"Loan payback S5E11 - ScatterHist {var_x} vs {var_y}",
+                     savefig=savefig)
+
+    
 
 
 # Scout theme: "Always leave the campsite cleaner than you found it"
