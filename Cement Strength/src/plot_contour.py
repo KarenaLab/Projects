59441b319b70ab5f5be40906f3221632e459c1e5
Seb Karena <EKChikui@gmail.com>
2025-11-23 20:27:38 -0300

    Create plot_contour.py

---
+++ Cement Strength/src/plot_contour.py
@@ -0,0 +1,27 @@
+# Countour 2D [P426]
+
+
+# Insights, improvements and bugfix
+#
+
+
+# Libraries
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+
+# ----------------------------------------------------------------------
+def plot_countour():
+    """
+    Description of the function
+    Information about variables and **kwargs
+
+    """
+    pass
+
+    return None
+
+
+# end
