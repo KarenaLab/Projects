9a8061e8b9af257ff80e5a29d638c7121cf212bc
Seb Karena <EKChikui@gmail.com>
2025-11-25 20:19:22 -0300

    Update loan_payback_00_eda.py

--- Loan Payback/loan_payback_00_eda.py
+++ Loan Payback/loan_payback_00_eda.py
@@ -23,9 +23,26 @@ from src.loan_payback_tools import (load_dataset, organize_report,
 from src.plot_histbox import plot_histbox
 from src.plot_scatterhist import plot_scatterhist
 from src.plot_heatmap import plot_heatmap
+from src.plot_barh import plot_barh
 
 
 # Functions
+def brief_category(Series):
+    data = np.array(Series)
+    total = data.size
+
+    counter = pd.DataFrame(data=[])
+    for i in np.unique(data):
+        count = (data == i).sum()
+        pct = np.round((count / total) * 100, decimals=3)
+
+        counter.loc[i, "count"] = count
+        counter.loc[i, "pct"] = pct
+
+    counter = counter.sort_values(by="count", ascending=False)
+
+
+    return counter
 
 
 
@@ -63,11 +80,18 @@ for var_x in cols_numeric():
                      mark_size=15, color="darkred", title=f"Loan payback S5E11 - ScatterHist {var_x} vs {target}",
                      savefig=savefig)
 
-"""
+
 # Heatmap
 plot_heatmap(sample, columns=cols_numeric(), title=f"Loan payback S5E11 - Heatmap",
              savefig=savefig)
-    
+
+"""
+for col in cols_categoric():
+    info = brief_category(df[col])
+    plot_barh(x=info.index, width=info["count"], title=f"Loan payback S5E11 - Category {col}", savefig=savefig)
+       
+        
+
 
 
 # Scout theme: "Always leave the campsite cleaner than you found it"
