f4b92ad8a1da5ee9f8223f3857233d9e8f323256
Seb Karena <EKChikui@gmail.com>
2025-11-26 20:03:04 -0300

    Create PaperCO - reference.py

---
+++ Paper prediction/PaperCO - reference.py
@@ -0,0 +1,28 @@
+# Project Paper CO
+
+# Libraries
+import os
+
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+
+import matplotlib.pyplot as plt
+
+
+# Project libraries
+
+
+
+# Functions
+
+
+
+# Setup/Config
+
+
+
+# Program ---------------------------------------------------------------
+
+
+
