e5126927bffe94a3471d1cbf9e909303fe1682b6
Seb Karena <EKChikui@gmail.com>
2025-11-26 20:28:11 -0300

    Create paperco_00_eda.py

---
+++ Paper prediction/paperco_00_eda.py
@@ -0,0 +1,25 @@
+# Project Paper CO
+
+# Libraries
+import os
+
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+
+import matplotlib.pyplot as plt
+
+
+# Project libraries
+from src.paper_co_tools import load_dataset
+
+
+# Functions
+
+
+
+# Setup/Config
+
+
+
+# Program ---------------------------------------------------------------
