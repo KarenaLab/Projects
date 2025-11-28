fec9cde040995152cb0f8c0ad2b72f90e437fd4c
Seb Karena <EKChikui@gmail.com>
2025-11-27 21:35:43 -0300

    Update paperco_00_eda.py

--- Paper prediction/paperco_00_eda.py
+++ Paper prediction/paperco_00_eda.py
@@ -2,6 +2,7 @@
 
 # Libraries
 import os
+import itertools
 
 import numpy as np
 import pandas as pd
@@ -13,7 +14,9 @@ import matplotlib.pyplot as plt
 # Project libraries
 from src.paper_co_tools import (load_dataset, organize_report)
 from src.plot_histbox import plot_histbox
-
+from src.plot_barv import plot_barv
+from src.plot_scatterhist import plot_scatterhist
+from src.plot_heatmap import plot_heatmap
 
 
 # Functions
@@ -45,26 +48,87 @@ def check_failure_data(DataFrame):
 
     return errors
 
+
+def remove_cols_unique(DataFrame, verbose=False):
+    """
+
+
+    """
+    cols_remove = list()
+    for col in DataFrame.columns:
+        if(DataFrame[col].nunique() == 1):
+            cols_remove.append(col)
+
+            if(verbose == True):
+                print(f" > Column '{col}' removed due unique item")
+
+
+    DataFrame = DataFrame.drop(columns=cols_remove)
+
+    return DataFrame
+
+
+def feat_eng_runtime_inv(DataFrame):
     
+    for asset in DataFrame["asset_id"].unique():
+        info = DataFrame.groupby(by="asset_id").get_group(asset)
+        runtime_max = info["runtime"].max()
+        info["runtime_inv"] = info["runtime"].apply(lambda x: x - runtime_max)
+
+        for i in info.index:
+            DataFrame.loc[i, "runtime_inv"] = info.loc[i, "runtime_inv"]
+
+
+    return DataFrame
+    
+
+        
+  
 # Setup/Config
 path_main = os.getcwd()
 path_database = os.path.join(path_main, "database")
 path_report = os.path.join(path_main, "report")
 
-SAVEFIG = False
+SAVEFIG = True
 
 
 # Program ---------------------------------------------------------------
 df = load_dataset(filename="pm_train.txt", path=path_database)
 
-# asset_id and runtime data validation
+# Asset_id and runtime data validation
 errors = check_failure_data(df)
 
 
-# Single variable analysis
+# DataFrame preparation
+cols_unique = pd.DataFrame(data=[])
 for col in df.columns:
-    plot_histbox(data=df[col], title=f"Paper CO - Histogram - {col}", savefig=True)
-    
+    cols_unique.loc[col, "unique"] = df[col].nunique()
+
+#plot_barv(x=cols_unique.index, height=cols_unique["unique"], xrotation=True,
+#          title=f"Paper CO - Unique values per variable", savefig=False)
+
+df = remove_cols_unique(df, verbose=False)
+df = feat_eng_runtime_inv(df)
+df_sample = df.sample(frac=.25, random_state=314)
+
+
+# Univariate analysis
+for col in df.columns:
+    plot_histbox(data=df_sample[col], title=f"Paper CO - Histogram - {col}", savefig=SAVEFIG)
+
+
+# Bivariate analysis
+cols_comb = list(itertools.combinations(list(df.columns), 2))
+for (var_x, var_y) in cols_comb:
+    plot_scatterhist(x=df_sample[var_x], y=df_sample[var_y], xlabel=var_x, ylabel=var_y, mark_size=15,
+                     title=f"Paper CO - Scatter - {var_x} vs {var_y}", savefig=SAVEFIG)
+
+plot_heatmap(df_sample, title="Paper CO - Heatmap", savefig=SAVEFIG)
+
+
+
+
+
 
 # Scout theme: "Always leave the campsite cleaner than you found it"
 organize_report(src=path_main, dst="report")
