e42c272352b1d59f374fd42093a1f9b2f00c3752
Seb Karena <EKChikui@gmail.com>
2025-11-29 09:21:26 -0300

    Update paperco_01_knn.py

--- Paper prediction/paperco_01_knn.py
+++ Paper prediction/paperco_01_knn.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 
 # Project libraries
 from src.paper_co_tools import (load_dataset, remove_cols_unique, feat_eng_runtime_inv,
-                                add_failure_tag, organize_report)
+                                add_failure_tag, remove_df_columns, organize_report)
 
 from src.plot_histbox import plot_histbox
 from src.plot_barv import plot_barv
@@ -41,43 +41,9 @@ def prepare_dataset(filename, path):
     data = add_failure_tag(data, threshold=-20)
 
     cols_remove = ["asset_id", "runtime", "tag_6", "runtime_inv"]
-    data = remove_columns(data, columns=cols_remove)
+    data = remove_df_columns(data, columns=cols_remove)
 
     return data
-
-
-def remove_columns(DataFrame, columns, verbose=False):
-    """
-    Remove columns that will not be helpful for the models.
-
-    Arguments:
-    * DataFrame: Pandas dataframe of project,
-    * columns: Columns names to be removed from,
-    * verbose: True or False* (default=False),
-
-    Return:
-    * DataFrame: Processed Pandas DataFrame
-
-    """
-    # Columns names preparation
-    cols_remove = list()
-    cols_dataframe = list(DataFrame.columns)
-
-    for col in columns:
-        if(cols_dataframe.count(col) == 1):
-            cols_remove.append(col)
-
-        else:
-            if(verbose == True):
-                print(f" > column '{col}' does NOT exists")
-
-    if(len(cols_remove) > 0):
-        DataFrame = DataFrame.drop(columns=cols_remove)
-
-
-    return DataFrame
-        
-    
 
    
 # Setup/Config ----------------------------------------------------------
