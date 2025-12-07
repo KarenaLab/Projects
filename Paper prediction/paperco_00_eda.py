8ca1b7b9b31d8ea003e8771a904615fe14e23670
Seb Karena <EKChikui@gmail.com>
2025-12-07 11:20:20 -0300

    Update paperco_00_eda.py

--- Paper prediction/paperco_00_eda.py
+++ Paper prediction/paperco_00_eda.py
@@ -20,6 +20,7 @@ from src.plot_histbox import plot_histbox
 from src.plot_barv import plot_barv
 from src.plot_scatterhist import plot_scatterhist
 from src.plot_heatmap import plot_heatmap
+from src.plot_line import plot_line
 
 
 # Functions
@@ -82,6 +83,14 @@ def plot_columns_nunique(DataFrame, title=None, savefig=False):
 
     return None
 
+
+def cols_tag():
+    cols = ["tag_2", "tag_3", "tag_4", "tag_6", "tag_7", "tag_8", "tag_9",
+            "tag_11", "tag_12", "tag_13", "tag_14", "tag_15", "tag_17",
+            "tag_20", "tag_21"]
+
+    return cols
+
     
 # Setup/Config ----------------------------------------------------------
 path_main = os.getcwd()
@@ -103,13 +112,13 @@ errors = check_failure_data(df)
 
 # DataFrame preparation:
 # >>> Check/Remove columns with unique values
-plot_columns_nunique(df, title=f"Paper CO - Unique values per variable", savefig=SAVEFIG)
+#plot_columns_nunique(df, title=f"Paper CO - Unique values per variable", savefig=SAVEFIG)
 df = remove_cols_unique(df, verbose=False)
 
 # >>> Feature Engineering with runtime as countdown to failure
 df = feat_eng_runtime_inv(df)
 
-"""
+
 # EDA Plots
 # Due dataprocessing visualization ONLY, using a slice of 25% of data
 df_sample = df.sample(frac=.25, random_state=314)
@@ -126,7 +135,47 @@ for (var_x, var_y) in cols_comb:
 
 plot_heatmap(df_sample, title="Paper CO - Heatmap", savefig=SAVEFIG)
 
-"""
+# >>> Asset ID
+for i in df["asset_id"].unique():
+    info = df.groupby(by="asset_id").get_group(i)
+
+    for col in cols_tag():
+        plot_line(x=info.index, y=info[col], title=f"Paper CO - Tag line Asset {i} - {col}", savefig=True)
+
+# >>> 
+for col in cols_tag():
+    title = f"Paper CO - Tag lines - {col}"
+    fig = plt.figure(figsize=[6, 3.375])        # Widescreen [16:9]
+    fig.suptitle(title, fontsize=10, fontweight="bold", x=0.98, ha="right")
+
+    for i in df["asset_id"].unique():
+        info = df.groupby(by="asset_id").get_group(i)
+        plt.plot(info["runtime"], info[col], linewidth=0.3, zorder=20)
+
+    plt.grid(axis="both", color="lightgrey", linestyle="--", linewidth=0.5, zorder=10)
+    
+    plt.tight_layout()
+    plt.savefig(title, dpi=320)
+    plt.show()
+
+
+for col in cols_tag():
+    title = f"Paper CO - Tag lines with smooth SMA - {col}"
+    fig = plt.figure(figsize=[6, 3.375])        # Widescreen [16:9]
+    fig.suptitle(title, fontsize=10, fontweight="bold", x=0.98, ha="right")
+
+    for i in df["asset_id"].unique():
+        info = df.groupby(by="asset_id").get_group(i)
+        info[col] = info[col].rolling(window=5).mean()
+        plt.plot(info["runtime"], info[col], linewidth=0.3, zorder=20)
+
+    plt.grid(axis="both", color="lightgrey", linestyle="--", linewidth=0.5, zorder=10)
+    
+    plt.tight_layout()
+    plt.savefig(title, dpi=320)
+    plt.show() 
+   
+
 # Scout theme: "Always leave the campsite cleaner than you found it"
 organize_report(src=path_main, dst="report")
 
